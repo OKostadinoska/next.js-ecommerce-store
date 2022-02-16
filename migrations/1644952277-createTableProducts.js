c0175e0f15d064f61f6380856d75dc93d8561f4e
Olivera Kostadinoska <kostadinoskaolivera@outlook.com>
2022-02-16 13:09:44 +0100

    Added migration folder

---
+++ migrations/1644952277-createTableProducts.js
@@ -0,0 +1,17 @@
+exports.up = async (sql) => {
+  // <insert magic here>
+  await sql`
+		CREATE TABLE products (
+			id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+			name varchar(20) NOT NULL,
+			type varchar(30) NOT NULL
+		);
+	`;
+};
+
+exports.down = async (sql) => {
+  // just in case...
+  await sql`
+	DROP TABLE products
+	`;
+};
