e58d7c067eec753d8b6d00ad6c0ee0abc8302c9b
Olivera Kostadinoska <kostadinoskaolivera@outlook.com>
2022-02-23 16:48:13 +0100

    Cleaned database comments

--- util/database.ts
+++ util/database.ts
@@ -50,32 +50,3 @@ export async function getProductById(id: number) {
   `;
   return camelcaseKeys(product);
 }
-
-// const productsDatabase = [
-//   {
-//     // id: '1',
-//     name: 'Vinyl Player',
-//     price: 47,
-//     type: 'music tool',
-//   },
-//   {
-//     // id: '2',
-//     name: 'Guitar',
-//     price: 4,
-//     type: 'music instrument',
-//   },
-//   {
-//     // id: '3',
-//     name: 'Saxophone',
-//     price: 9,
-//     type: 'music instrument',
-//   },
-//   {
-//     // id: '4',
-//     name: 'Microfon',
-//     price: 2,
-//     type: 'music tool',
-//   },
-// ];
-
-// export default productsDatabase;
